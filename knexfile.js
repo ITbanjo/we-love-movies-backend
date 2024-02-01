479345963663c3b48e2d6c7a5862a7c6444453ab
Isaac Taylor <banjertaylor@gmail.com>
2024-02-01 17:01:00 -0500

    Movies routes completed / error handler functions added / fixed issue with migrations

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,14 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL_DEV = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL_PROD,
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: DATABASE_URL_DEV,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -21,7 +22,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: DATABASE_URL_PROD,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
